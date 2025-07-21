42ae77e9a6a2b8bda639e31fd5ff78028a0cc0f9
Mean Un <mean.un.personal@gmail.com>
2025-07-21 10:33:37 +0700

    create all attibute of tables

--- booking_travel_api/database/migrations/2025_07_21_023952_create_reviews_table.php
+++ booking_travel_api/database/migrations/2025_07_21_023952_create_reviews_table.php
@@ -11,10 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+Schema::create('reviews', function (Blueprint $table) {
+    $table->id('review_id');
+    $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+    $table->string('entity_type');
+    $table->unsignedBigInteger('entity_id');
+    $table->integer('rating');
+    $table->text('comment')->nullable();
+    $table->date('review_date');
+    $table->timestamps();
+
+    $table->index(['entity_type', 'entity_id']);
+});
     }
 
     /**
