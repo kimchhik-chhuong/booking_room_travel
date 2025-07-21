6539d334bc9753fd5155e6104a5047357a8fcd0a
sreyletsao <sreylet.sao@student.passerellesnumeriques.org>
2025-07-21 09:43:35 +0700

    Create all table

---
+++ booking_travel_api/app/Http/Controllers/HotelMetadataController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HotelMetadata;
+use Illuminate\Http\Request;
+
+class HotelMetadataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HotelMetadata $hotelMetadata)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HotelMetadata $hotelMetadata)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HotelMetadata $hotelMetadata)
+    {
+        //
+    }
+}
