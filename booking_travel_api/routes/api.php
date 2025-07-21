175bae2ab499ba9f54407fc64a7dc76a2323edaa
kimchhik-chhuong <kimchhik.chhuong@student.passerellesnumeriques.org>
2025-07-21 16:21:29 +0700

    fix-api

--- booking_travel_api/routes/api.php
+++ booking_travel_api/routes/api.php
@@ -8,4 +8,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+//travelers
 Route::get('/travelers', [TravelerController::class, 'index']);
+Route::get('/travelers/{id}', [TravelerController::class, 'show']);
+Route::post('/travelers', [TravelerController::class, 'store']);
+Route::put('/travelers/{id}', [TravelerController::class, 'update']);
+Route::delete('/travelers/{id}', [TravelerController::class, 'destroy']);
+
+//
