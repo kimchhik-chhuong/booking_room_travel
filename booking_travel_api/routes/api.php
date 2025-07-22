1c6fc86a77311f0f82a1d140cd5d49b5c40dbfa3
sreyletsao <sreylet.sao@student.passerellesnumeriques.org>
2025-07-22 14:26:20 +0700

    get login and resjester

--- booking_travel_api/routes/api.php
+++ booking_travel_api/routes/api.php
@@ -1,11 +1,16 @@
 <?php
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\DestinationController;
 use App\Http\Controllers\TravelerController;
+use App\Models\Booking;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
+Route::apiResource('/users', UserController::class);
+Route::apiResource('/booking', Bookingcontroller::class);
+Route::apiResource('/destination', Destinationcontroller::class);
 Route::get('/travelers', [TravelerController::class, 'index']);
