2f0091a39a0ccc427c0925f3f6f935f14795fbcb
sreyletsao <sreylet.sao@student.passerellesnumeriques.org>
2025-07-28 08:16:50 +0700

    fix

--- booking_travel_api/routes/api.php
+++ booking_travel_api/routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
